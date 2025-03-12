09983cf71fad09418459aa97b878d7a2cd799574
Riku329 <caseyventurinatio@gmail.com>
2025-03-13 09:54:04 +1300

    Update version_control.py

--- version_control.py
+++ version_control.py
@@ -4,3 +4,10 @@ print("Hello, World")
 # Version 2
 name = input("Enter Your Name:")
 print(f"Hello,{name}!")
+
+# Version 3
+while True:
+    name = input("Enter your name (or 'exit' to quit):")
+    if name.lower() =='exit':
+        break
+    print(f"Hello, {name}!")  
